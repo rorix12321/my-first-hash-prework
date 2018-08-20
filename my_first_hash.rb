7f6da8cbec2d90ff7d0a4e3c160001d81e5c8072
Ricky Liu <rorix12321@gmail.com>
2018-08-20 12:35:27 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,11 +1,21 @@
 def my_hash
-wow = {"class" => "demonhunter"}
+  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = {"Karna" => "Vasavi Shakhti"}
 
 end
 
 
 def shipping_manifest
-the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  # set a variable called the_manifest, equal to a hash
+  # fill that hash with key/value pairs that describe the following information:
+  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
+
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
+
 end
 
 def retrieval
@@ -14,7 +24,10 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-return shipping_manifest["oil paintings"]
+
+  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+
+  shipping_manifest["oil paintings"]
 
 end
 
@@ -24,7 +37,17 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-shipping_manifest["muskets"] = 2
-shipping_manifest["gun powder"] = 4
-return shipping_manifest
+  # add 2 muskets to the shipping_manifest hash below
+
+  shipping_manifest["muskets"] = 2
+
+
+  # add 4 gun powder to the shipping_manifest hash below
+
+  shipping_manifest["gun powder"] = 4
+
+  # return the shipping_manifest hash below
+
+  shipping_manifest
+
 end
